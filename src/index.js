f5a9c367eef644d803c5cbb7f188e3ca3376f0c2
iyatan <iyatan.atchoro@mail.mcgill.ca>
2019-09-08 23:21:16 -0400

    sign up form1

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ firebase.initializeApp({
 const routing = (
     < Router >
         <div id='routing-container'>
-            <Route path="./login" component={LoginComponent}></Route>
-            <Route path="./signup" component={SignupComponent}></Route>
-            <Route path="./dashboard" component={DashboardComponent}></Route>
+            <Route exact path="/login" component={LoginComponent}></Route>
+            <Route exact path="/signup" component={SignupComponent}></Route>
+            <Route exact path="/dashboard" component={DashboardComponent}></Route>
         </div>
 
     </Router >
