a38f5d76244248eae372cc9cd446eab0f57a805f
iyatan <iyatan.atchoro@mail.mcgill.ca>
2019-09-22 23:45:22 -0400

    CHAT

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import ChatListComponent from '../chatlist/chatList'
 
 class DashboardComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
     render() {
-        return (<div>Hello word from Dashboard</div>);
+
+        return (
+            <div>
+                <div>Hello word from Dashboard</div>
+                <ChatListComponent history={this.props.history}></ChatListComponent>
+            </div>
+
+        );
     }
 }
 export default DashboardComponent;
