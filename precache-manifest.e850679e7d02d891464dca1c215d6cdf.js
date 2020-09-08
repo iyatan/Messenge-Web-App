9577ad731761e62357c69b486db9965205911633
Iyatan Atchoro <iyatan.atchoro@mail.mcgill.ca>
2020-09-08 01:38:19 -0400

    Updates

---
+++ precache-manifest.e850679e7d02d891464dca1c215d6cdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf13168c5089169cc7beed39d12a93b",
+    "url": "/Messenger-Web-App/index.html"
+  },
+  {
+    "revision": "5d7c7a46053520113f7d",
+    "url": "/Messenger-Web-App/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff095604da996711f905",
+    "url": "/Messenger-Web-App/static/js/2.4268077e.chunk.js"
+  },
+  {
+    "revision": "5d7c7a46053520113f7d",
+    "url": "/Messenger-Web-App/static/js/main.3e88450a.chunk.js"
+  },
+  {
+    "revision": "bfb579145899a6e42686",
+    "url": "/Messenger-Web-App/static/js/runtime~main.ae305025.js"
+  }
+]);
